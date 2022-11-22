6daf49f84298fe2c04865d0e820d10385bf49518
Vladislav Milko <vladmilko27@gmail.com>
2022-11-23 00:27:55 +0300

    Добавление енумов

---
+++ src/enums/ColorTypes.ts
@@ -0,0 +1,4 @@
+export enum ColorTypes {
+  WHITE = 'white',
+  BLACK = 'black',
+}
---
+++ src/enums/FigureNames.ts
@@ -0,0 +1,9 @@
+export enum FigureNames {
+  FIGURE = 'Фигура',
+  KING = 'Король',
+  QUEEN = 'Ферзь',
+  KNIGHT = 'Конь',
+  PAWN = 'Пешка',
+  ROOK = 'Ладья',
+  BISHOP = 'Слон',
+}
